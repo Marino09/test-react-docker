aac410318bc9b3e97e96d9561aac2f82fc63bab6
Marino Martinez <me.martinezp9@gmail.com>
2021-08-11 19:49:29 -0400

    adding .dockerignore

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Counter extends React.Component {
+  constructor(props) {
+      super(props);
+      this.handleAddOne = this.handleAddOne.bind(this);
+      this.handleMinusOne = this.handleMinusOne.bind(this);
+      this.handleReset = this.handleReset.bind(this);
+      this.state = {
+          count: props.count
+      };
+  }
+
+  handleAddOne() {
+      this.setState((prevState) => {
+          return {
+              count: prevState.count + 1
+          }
+      })
+  }
+
+  handleMinusOne() {
+      this.setState((prevState) => {
+          return {
+              count: prevState.count - 1
+          }
+      })
+  }
+
+  handleReset(){
+      this.setState(() => {
+          return {
+              count: 0
+          }
+      });
+  }
+  render() {
+      return (
+      <div>
+          <h1>count: {this.state.count}</h1>
+          <button onClick={this.handleAddOne}>+1</button>
+          <button onClick={this.handleMinusOne}>-1</button>
+          <button onClick={this.handleReset}>Reset</button>
+      </div>
+      );
+  }
 }
 
-export default App;
+Counter.defaultProps = {
+  count: 0
+}
+
+// Create three methods: handleAddOne, handleMinusOne, handleReset
+// use console.log to print the method name and
+// wire up onClick & bind in the constructor
+ReactDOM.render(<Counter count={5}/>, document.getElementById('root'));
+export default Counter;
+
